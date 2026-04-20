691a0b02da9d181c92bd262b03182447bf9567d1
Mamello <mamello695@gmail.com>
2026-04-20 14:51:11 +0200

    First commit- BMI Calculator

---
+++ BmiCalculator.java
@@ -0,0 +1,18 @@
+package bmiCalculator;
+
+import.java.util.Scanner;
+import java.util.Locale;
+
+public class BmiCalculator{
+
+
+
+public static void main(String[] args){
+	Scanner s = new Scanner(System.in);
+	scanner.useLocale(Locale.US);
+}
+
+
+
+
+}
